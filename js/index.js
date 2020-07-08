6dfb6481229872fdf8f49a20b63e3f9cf291f2a0
AmirGholizadeh <amir110ghoolizadeh@gmail.com>
2020-07-08 15:39:09 +0430

    add another way to show the result

--- js/index.js
+++ js/index.js
@@ -32,24 +32,32 @@
     label.addEventListener('click', e => {
       e.target.textContent = checkAndChange(e.target.textContent);
     });
-    
+
     elements.push(label.id, input.id);
     
     document.querySelector('.content').appendChild(label);
     document.querySelector('.content').appendChild(input);
   });
 
+  const showResult = () => {
+    try{
+      for(let i =0;i<inputs.length;i++){
+        if(document.getElementById(inputs[i]).value === '') throw 'please fill out the inputs';
+        if(isNaN(document.getElementById(inputs[i]).value)) throw 'please enter only numbers!';
+      }
+      result.textContent = eval(elements.map(el => document.getElementById(el).value || document.getElementById(el).textContent).join(''));
+    }catch(err){
+      alert(err);
+    }
+  };
+ 
   document.addEventListener('keypress', ev => {
     if(ev.key === 'Enter'){
-      try{
-        for(let i =0;i<inputs.length;i++){
-          if(document.getElementById(inputs[i]).value === '') throw 'please fill out the inputs';
-          if(isNaN(document.getElementById(inputs[i]).value)) throw 'please enter only numbers!';
-        }
-        result.textContent = eval(elements.map(el => document.getElementById(el).value || document.getElementById(el).textContent).join(''));
-      }catch(err){
-        alert(err);
-      }
+      showResult();
     }
+  });
+  
+  result.addEventListener('click', ev => {
+    showResult();
   });
 })();
